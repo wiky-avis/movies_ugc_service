22b16bbd640336e5c4fdc044d72121c27369ebed
opodryadov <oleg_podryadov@mail.ru>
2023-05-26 21:51:24 +0500

    Реализована ёлка (ELK) приложения

--- src/app.py
+++ src/app.py
@@ -1,19 +1,32 @@
 import sys
+from uuid import uuid4
 
 import uvicorn as uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi_pagination import add_pagination
 
 from src.api import v1
 from src.brokers.kafka_producer import KafkaProducer
 from src.common.db import MongoDbConnector
+from src.common.tracer import configure_tracer
 from src.containers import Container
 from src.settings import logger, settings
+
+
+def init_sentry():
+    if settings.enable_sentry:
+        import sentry_sdk
+        from sentry_sdk.integrations.fastapi import FastApiIntegration
+
+        sentry_sdk.init(integrations=[FastApiIntegration()])
 
 
 def create_app() -> FastAPI:
     container = Container()
     container.wire(modules=[sys.modules[__name__]])
+
+    init_sentry()
 
     app = FastAPI(
         on_startup=[KafkaProducer.setup, MongoDbConnector.setup],
@@ -26,12 +39,36 @@ def create_app() -> FastAPI:
     app.container = container  # type: ignore
     add_pagination(app)
 
+    if settings.enable_tracer:
+        configure_tracer(app)
+
     app.include_router(v1.router)
 
     return app
 
 
 app = create_app()
+
+
+@app.middleware("http")
+async def validate_request_id_header(request: Request, call_next):
+    request_id = request.headers.get("X-Request-Id")
+    if request_id is None:
+        if settings.debug:
+            request.headers.__dict__["_list"].append(
+                (
+                    "x-request-id".encode(),
+                    f"{str(uuid4())}".encode(),
+                )
+            )
+        else:
+            return JSONResponse(
+                content={"message": "Missing X-Request-Id header"},
+                status_code=400,
+            )
+
+    response = await call_next(request)
+    return response
 
 
 if __name__ == "__main__":
--- src/settings/app.py
+++ src/settings/app.py
@@ -6,10 +6,18 @@ load_dotenv()
 
 
 class AppSettings(BaseSettings):
+    debug: bool = Field(env="DEBUG", default=False)
+
     project_host: str = Field(env="PROJECT_HOST", default="0.0.0.0")
     project_port: int = Field(env="PROJECT_PORT", default=8000)
 
     log_format: str = Field(env="LOG_FORMAT", default="INFO")
+
+    enable_sentry: bool = Field(env="ENABLE_SENTRY", default=False)
+
+    enable_tracer: bool = Field(env="ENABLE_TRACER", default=False)
+    jaeger_host: str = Field(env="JAEGER_HOST", default="jaeger")
+    jaeger_port: int = Field(env="JAEGER_PORT", default=6831)
 
     class Config:
         env_file: str = ".env"
