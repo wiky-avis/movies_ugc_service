8df4fcc95dba68f19c2338e0c1d0295d9e3b99c0
opodryadov <oleg_podryadov@mail.ru>
2023-05-31 22:33:45 +0500

    Добавил таблицу и ключ шардирования

--- scripts/enable-sharding.js
+++ scripts/enable-sharding.js
@@ -1,4 +1,4 @@
 use ugc
 sh.enableSharding("ugc")
 db.adminCommand( { shardCollection: "ugc.view_progress", key: { film_id: "hashed" } } )
-db.adminCommand( { shardCollection: "ugc.user_bookmarks", key: { film_id: "hashed" } } )
+db.adminCommand( { shardCollection: "ugc.user_film_reviews", key: { review_id: "hashed" } } )
