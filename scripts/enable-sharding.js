d3bf9879b46a3211c29014e85c24e6f91edbd1f2
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-06-03 13:44:17 +0300

    лайки/дизлайки

--- scripts/enable-sharding.js
+++ scripts/enable-sharding.js
@@ -4,3 +4,4 @@ db.adminCommand( { shardCollection: "ugc.view_progress", key: { film_id: "hashed
 db.adminCommand( { shardCollection: "ugc.user_bookmarks", key: { film_id: "hashed" } } )
 db.adminCommand( { shardCollection: "ugc.user_film_reviews", key: { review_id: "hashed" } } )
 db.adminCommand( { shardCollection: "ugc.film_scores", key: { film_id: "hashed" } } )
+db.adminCommand( { shardCollection: "ugc.user_film_reviews_likes", key: { review_id: "hashed" } } )
